c981cf8cb52b77774ef5a80729ba27540d53f3e7
ChotanSharma <chotansharma@yahoo.com>
2022-04-21 17:06:21 -0400

    add the change

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,9 +4,17 @@ import React from 'react';
 
 function About() {
   return (
-    <section>
-      <h1 id="about">Who am I?</h1>
-    </section>
+    <section className="my-5">
+    <h1 id="about">Who am I?</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Nibh cras pulvinar mattis nunc sed blandit. Mi ipsum faucibus vitae aliquet nec ullamcorper. 
+        Facilisis sed odio morbi quis.
+        <br></br>
+        <br></br>
+        Velit dignissim sodales ut eu sem integer. Scelerisque in dictum non consectetur a erat. 
+        Donec et odio pellentesque diam volutpat. Blandit turpis cursus in hac habitasse platea. Rutrum tellus pellentesque eu tincidunt tortor aliquam. Blandit libero volutpat sed cras ornare arcu dui vivamus. Venenatis tellus in metus vulputate eu. Arcu felis bibendum ut tristique. Scelerisque fermentum dui faucibus in ornare quam viverra orci. Tempor nec feugiat nisl pretium fusce id velit ut tortor. Facilisi nullam vehicula ipsum a arcu cursus vitae. Leo vel fringilla est ullamcorper eget. 
+        Ut ornare lectus sit amet est placerat in. Aenean et tortor at risus viverra adipiscing at.</p>
+  </section>
   );
 }
 
